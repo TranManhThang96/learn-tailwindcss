13797265aa695965fb1b3a258ade9385cfc8af47
TranManhThang <tranmanhthang96@gmail.com>
2023-12-17 10:22:27 +0700

    learn tailwindcss

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <button className="... bg-sky-500 hover:bg-sky-200 focus:bg-sky-200">
+        Save changes
+      </button>
+    </>
+  )
+}
+
+export default App
